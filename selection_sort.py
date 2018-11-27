ea382cb0b2fa44d410755cba89a1d4d1e5bbb1d7
Akhilendra <akhilendra44@gmail.com>
2018-11-27 23:06:19 +0530

    selection sort

--- selection_sort.py
+++ selection_sort.py
@@ -1,16 +1,17 @@
+data =[43,54,23,22,65,54,44]
 
-data = [2,4,3,67,43,23,7]
-
-def bubble_sort(data):
-
-    for n in range(len(data)-1,0,-1):
-
-        for i in range(n):
-            if data[i]>data[i+1]:
-                temp=data[i]
-                data[i]=data[i+1]
-                data[i+1]=temp
-    
+def selection_sort(data):
+   
+    for n in range(len(data)-1):
+        minpos=n
+        for i in range(n, len(data)):
+            if data[i]<data[minpos]:
+                minpos=i
+        
+        temp =  data[n]
+        data[n]=data[minpos]
+        data[minpos]=temp
+        
     return data
 
-print(bubble_sort(data))
+print(selection_sort(data))
