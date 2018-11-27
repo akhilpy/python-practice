9b6a53ac3cb480ffad4deef48e42440e92778c95
Akhilendra <akhilendra44@gmail.com>
2018-11-27 22:49:15 +0530

    bubble sort

---
+++ bubble.py
@@ -0,0 +1,14 @@
+
+data = [2,4,3,67,43,23,7]
+
+def bubble_sort(data):
+    for n in range(len(data)-1,0,-1):
+        for i in range(n):
+            if data[i]>data[i+1]:
+                temp=data[i]
+                data[i]=data[i+1]
+                data[i+1]=temp
+    
+    return data
+
+print(bubble_sort(data))
