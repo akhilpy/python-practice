3d5de6703ee724dd7a9590af6c55c8f816c8cdcc
akhilpy <akhilendra44@gmail.com>
2019-02-11 11:15:43 +0530

    binary Expression evaluation

---
+++ binary_expression.py
@@ -0,0 +1,28 @@
+"""This example convert the binary string expression0 to  int and calculate the result """
+
+stri = '101+100-110*101010/101'
+
+total_count = 0
+for i,j in enumerate(stri):
+    if j in "+-/*":      # count the total mathematical expression 
+        total_count = total_count + 1
+
+
+result = []
+k=0
+count = 0
+for i,j in enumerate(stri):
+    if j in "+-/*":
+       result.append(str(int(stri[k:i],2)))  # Get the binary expression  and convert into INT and append in the Result list
+
+       result.append(j) # Append the mathematical expression in Result list
+       k = i+1 
+       count = count+1 
+
+    if count == total_count: 
+       result.append(str(int(stri[i+1:],2))) # Append the last binary expression in the Result List
+       count = count+1
+
+print(result) #The complete list of Int value and mathematical expression in the order of given string.
+
+print(eval(('').join(result))) # Join the list and eval() the Result.
